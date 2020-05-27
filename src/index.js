4f5ada538c095adfe70d5b6d7ad7fb914a3c48f1
Jayashri <jayashri1394@gmail.com>
2020-05-27 16:00:12 +0200

    redux is added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configureStore} from "./store";
+import {persistStore} from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import {Provider} from 'react-redux';
 import App from './App.js';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+const store = configureStore();
+const persistor = persistStore(store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate
+            loading={<div>Loading...</div>}
+            persistor={persistor}>
+        <App/>
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root')
+);
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './TodoListItem.css';
 import './TodoList.css';
 
-const TodoListItem = ({todo}) =>(
+const TodoListItem = ({todo,onRemovePressed}) =>(
     <div className={"todo-item-container"}>
         <h3>{todo.text}</h3>
         <div className={"button-container"}>
-            <button className={"completed-button"}>Mark as Completed</button>
+            <button
+                onClick={()=> onRemovePressed(todo.text)}
+                className={"completed-button"}>Mark as Completed</button>
             <button className={"remove-button"}>Remove</button>
         </div>
     </div>
